1a1cdd26d9a96a01006a7070c6b492bd420e8d5c
David Roth <david.roth@students.bfh.ch>
2025-08-26 12:17:33 +0200

    Working on implementing the gui with the room manager

--- client.py
+++ client.py
@@ -1,32 +1,75 @@
-from tkinter import * # pyright: ignore[reportWildcardImportFromLibrary]
+import tkinter as tk
 from tkinter import ttk
 
 import rpyc
+import uuid
+
 c = rpyc.connect("localhost", 18861)
 
-class Client():
-    def __init__(self):
-        self.create_window()
+class Client(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
 
-    def create_window(self):
-        root = Tk()
-        frm = ttk.Frame(root, padding=10)
-        frm.grid()
-        ttk.Label(frm, text="Py-toe-n").grid(column=0,row=0)
-        ttk.Button(frm, text="Create room", command=self.create_room).grid(column=0,row=1)
-        ttk.Button(frm, text="Join room", command=self.join_room).grid(column=0,row=2)
-        ttk.Button(frm, text="Help", command=self.show_help).grid(column=0,row=3)
-        root.mainloop()
+        container = ttk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
 
-    def create_room(self):
-        id = c.root.create_room()
-        print(f"RoomID: {id}")
+        self.frames = {}
+        for F in (MenuPage, RoomList):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
 
-    def join_room(self):
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("MenuPage")
+
+        self.client_id = uuid.uuid4()
+
+    def show_frame(self, name: str):
+        frame = self.frames[name]
+        frame.tkraise()
+
+    def show_room_list(self, root: tk.Tk) -> None:
         pass
 
-    def show_help(self):
+    def create_room(self) -> None:
+        id: uuid.UUID = c.root.create_room(self.client_id)
+
+    def join_room(self, room_id: uuid.UUID) -> None:
+        print(f"Join room {room_id}")
+
+    def show_help(self) -> None:
         pass
+
+class MenuPage(ttk.Frame):
+    def __init__(self, parent, controller):
+        ttk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        label = ttk.Label(self, text="Py-toe-n")
+        label.pack(side="top", fill="y", padx=5, pady=10)
+
+        ttk.Button(self, text="Create room", command=controller.create_room).pack(side="top")
+        ttk.Button(self, text="Join room", command=lambda: controller.show_frame("RoomList")).pack(side="top")
+        ttk.Button(self, text="Help", command=controller.show_help).pack(side="top")
+
+
+class RoomList(ttk.Frame):
+    def __init__(self, parent, controller):
+        ttk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        label = ttk.Label(self, text="Room List")
+        label.pack(side="top", fill="y", padx=5, pady=10)
+
+        rooms = c.root.get_rooms()
+
+        for room in rooms:
+            button = ttk.Button(self, text=f"{room['room_id']} - Oponent: {room['oponent']}", command=lambda: controller.join_room(room['room_id']))
+            button.pack(side="top")
 
 if __name__ == "__main__":
-    Client()
+    client = Client()
+    client.mainloop()
--- server.py
+++ server.py
@@ -2,30 +2,43 @@ import rpyc
 import uuid
 
 class TicTacToeService(rpyc.Service):
-    rooms = []
+    rooms: list[dict] = []
 
     def __init__(self):
-        self.a = 0
+        pass
 
-    def on_connect(self, conn):
+    def on_connect(self, conn) -> None:
         print("Client connected.")
-        pass
 
-    def on_disconnect(self, conn):
+    def on_disconnect(self, conn) -> None:
         print("Client disconnected.")
-        pass
 
-    def exposed_create_room(self):
+    def exposed_create_room(self, client_id: uuid.UUID) -> uuid.UUID:
         room_id = uuid.uuid4()
-        TicTacToeService.rooms.append(room_id)
+
+        room = {
+            "created_by": client_id,
+            "room_id": room_id
+        }
+
+        TicTacToeService.rooms.append(room)
+
+        print(f"Room '{room_id}' created by '{client_id}'")
 
         return room_id
     
-    def exposed_show_rooms(self):
+    def exposed_get_rooms(self) -> list[dict]:
         return TicTacToeService.rooms
     
-    def exposed_join_room(self, room_id: str):
-        pass
+    def exposed_join_room(self, room_id: uuid.UUID, client_id: uuid.UUID) -> bool:
+        for room in TicTacToeService.rooms:
+            if room["room_id"] == room_id:
+                room["oponent"] = client_id
+                
+                return True
+            
+        return False
+
 
 
 if __name__ == "__main__":
